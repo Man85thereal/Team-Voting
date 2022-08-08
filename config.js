5d2662ce6bb30a72ef13258d5b75fc722e16a53d
Man85thereal <96772344+Man85thereal@users.noreply.github.com>
2022-08-08 18:08:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase';
-
-// add SDK Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDIsIkwL-v1gxHTh6NPjEhSHypMlyTA-Pk",
+  authDomain: "voting-7cebe.firebaseapp.com",
+  databaseURL: "https://voting-7cebe-default-rtdb.firebaseio.com",
+  projectId: "voting-7cebe",
+  storageBucket: "voting-7cebe.appspot.com",
+  messagingSenderId: "975433141226",
+  appId: "1:975433141226:web:a53f8fe07abd346e57c093"
+};
 var firebaseConfig = {
 
 };
